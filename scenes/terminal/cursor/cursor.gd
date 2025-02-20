0722031a70a49a7e7421659d3e640d25cb3928a8
Ethan Christensen <etc@unknownstudio.com>
2025-02-19 22:30:22 -0700

    fix(cursor): fix cursor not starting at mouse pos

--- scenes/terminal/cursor/cursor.gd
+++ scenes/terminal/cursor/cursor.gd
@@ -3,7 +3,8 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	update()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +13,7 @@ func _process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouse:
-		global_position = get_global_mouse_position()
+		update()
+
+func update() -> void:
+	global_position = get_global_mouse_position()
